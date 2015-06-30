2810778be352da7d477340f290c0c401da591ed4
Motonari Tsuzuki <your.email@example.com>
2015-06-30 10:56:35 +0900

    d

--- db/migrate/20150629221915_rename_column_in_user.rb
+++ db/migrate/20150629221915_rename_column_in_user.rb
@@ -1,8 +1,8 @@
 class RenameColumnInUser < ActiveRecord::Migration
   def change
   	rename_column :microposts, :user_id, :uuid
-  	change_column :microposts, :uuid, :string, :limit => 36
-  	change_column :users, :uuid, :string, :limit => 36
+  	change_column :microposts, :uuid, :string
+  	change_column :users, :uuid, :string
   	change_column :microposts, :content, :text
   end
 end
