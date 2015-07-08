4aaa16df4b239dd07c287b733f734d59cf670673
Motonari Tsuzuki <your.email@example.com>
2015-07-08 14:56:50 +0900

    wow turn integer

--- db/migrate/20150629004156_add_id_to_users.rb
+++ db/migrate/20150629004156_add_id_to_users.rb
@@ -1,7 +1,7 @@
 class AddIdToUsers < ActiveRecord::Migration
   def change
     add_column :users, :donno, :boolean, default: false
-    add_column :users, :wow, :boolean, default: false
+    add_column :users, :wow, :integer, default: 0
     add_column :users, :room_id, :integer
     add_index :users, :uuid, unique: true
     add_index :users, :room_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708004715) do
+ActiveRecord::Schema.define(version: 20150701142315) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
